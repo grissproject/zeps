cd965243a6a375e47a64fefe206a0427c25b0b86
Javier Orellana <javier.orellana.vb@gmail.com>
2014-01-20 15:49:36 -0400

    Remove (comment out) the Bolivian office since there is no official place yet

--- layout/footer.php
+++ layout/footer.php
@@ -15,7 +15,7 @@
                 <li><label>Email:</label> izalles@zepsgroup.com</li>
             </ul>
         </div>
-        <div class="columns four offset-by-one">
+        <!--<div class="columns four offset-by-one">
             <ul>
                 <li class="title"><span>Bolivia Office</span></li>
                 <li>Av. America #435</li>
@@ -24,7 +24,7 @@
                 <li><label>Office:</label> 832-456-7910</li>
                 <li><label>Mobile:</label> 713-206-8974</li>
             </ul>
-        </div>
+        </div>-->
         <div class="clear"></div>
         <div id="copy">ZEPS, LLC &copy 2013  &nbsp; - &nbsp;  All Rights Reserved</div>
         <!--<div id="snail">Web Design &raquo; <img src="images/smartsnail.png" /></div>-->
